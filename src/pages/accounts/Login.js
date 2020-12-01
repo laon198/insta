a5aaf0ab2bf69233cb951bd33a4cbe5ecaff808e
park <issg2030@gmail.com>
2020-12-01 14:28:13 +0900

    gg

--- src/pages/accounts/Login.js
+++ src/pages/accounts/Login.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Contents from "../components/Contents";
+import Footer from "../components/Footer";
 
-export default function Login(){
-    return(
-        <div>
-            Login
-        </div>
-    );
+export default function Login() {
+  return (
+    <div>
+      <Contents></Contents>
+      <Footer></Footer>
+    </div>
+  );
 }
